0e4b6317730a21723cfe52ee82755aacbf71cee0
Cerulean09 <robin.dartanto@hotmail.com>
2025-07-01 23:54:44 +0700

    01072025-2

--- taratechapi/settings.py
+++ taratechapi/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,10 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'nocanpayments',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,10 +53,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True # or set a whitelist
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:4838",  # Your Flutter web dev origin
+# ]
+
 
 ROOT_URLCONF = 'taratechapi.urls'
 
