6a88059b528db9044cc0465a467f13b2b8b6e1ed
Cerulean09 <robin.dartanto@hotmail.com>
2025-11-03 23:07:04 +0700

    03112025

--- chongqinghotpot/views.py
+++ chongqinghotpot/views.py
@@ -1,34 +1,37 @@
 import json
-import http.client
 import requests
 from django.http import JsonResponse
 
-
 def get_token():
     """
-    Get Qontak OAuth access token using CRM username/password (no client_id needed).
+    Get Qontak OAuth access token using CRM username/password.
     """
     crm_username = 'callcenter@chongqinghotpot.id'
     password = 'PassWord88@00'
 
-    conn = http.client.HTTPSConnection("app.qontak.com")
-    payload = f'grant_type=password&username={crm_username}&password={password}'
-
+    url = "https://app.qontak.com/oauth/token"
     headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Accept': 'application/json',
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Accept": "application/json"
+    }
+    data = {
+        "grant_type": "password",
+        "username": crm_username,
+        "password": password
     }
 
-    conn.request("POST", "/oauth/token", payload, headers)
-    res = conn.getresponse()
-    data = res.read().decode("utf-8")
-
     try:
-        token_data = json.loads(data)
-        return token_data.get("access_token")
+        response = requests.post(url, headers=headers, data=data)
+        print("Token Response Code:", response.status_code)
+        print("Token Response Body:", response.text)
+
+        if response.status_code == 200:
+            token_data = response.json()
+            return token_data.get("access_token")
+
+        return None
     except Exception as e:
-        print("Error parsing token response:", e)
-        print("Raw token response:", data)
+        print("Error requesting token:", str(e))
         return None
 
 
@@ -46,7 +49,6 @@ def get_all_contacts(request):
         "Content-Type": "application/json",
     }
 
-    # You can include optional query parameters (example: ?page=1&per_page=25)
     params = {
         "page": 1,
         "per_page": 25,
@@ -59,5 +61,7 @@ def get_all_contacts(request):
     except Exception:
         data = {"error": response.text}
 
-    print("Response:", data)
+    print("Contacts Response Code:", response.status_code)
+    print("Contacts Response Body:", data)
+
     return JsonResponse(data, safe=False, status=response.status_code)
