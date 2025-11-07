fed0f6e09d42997c1247242e4332e49897ba3957
Cerulean09 <robin.dartanto@hotmail.com>
2025-11-07 23:19:34 +0700

    07112025-2

--- ecosuite/urls.py
+++ ecosuite/urls.py
@@ -12,11 +12,11 @@ urlpatterns = [
     path('update-user/<str:user_id>/', views.update_user, name='update_user'),
     
     # Brand Management Endpoints
-    path('admin/get-all-brands/', views.get_all_brands, name='get_all_brands'),
-    path('admin/create-brand/', views.create_brand, name='create_brand'),
-    path('admin/get-brand/<str:brand_id>/', views.get_brand, name='get_brand'),
-    path('admin/update-brand/<str:brand_id>/', views.update_brand, name='update_brand'),
-    path('admin/suspend-brand/<str:brand_id>/', views.suspend_brand, name='suspend_brand'),
-    path('admin/upsert-brand/', views.upsert_brand, name='upsert_brand'),
-    path('admin/upload-brand-logo/<str:brand_id>/', views.upload_brand_logo, name='upload_brand_logo'),
+    path('get-all-brands/', views.get_all_brands, name='get_all_brands'),
+    path('create-brand/', views.create_brand, name='create_brand'),
+    path('get-brand/<str:brand_id>/', views.get_brand, name='get_brand'),
+    path('update-brand/<str:brand_id>/', views.update_brand, name='update_brand'),
+    path('suspend-brand/<str:brand_id>/', views.suspend_brand, name='suspend_brand'),
+    path('upsert-brand/', views.upsert_brand, name='upsert_brand'),
+    path('upload-brand-logo/<str:brand_id>/', views.upload_brand_logo, name='upload_brand_logo'),
 ]
